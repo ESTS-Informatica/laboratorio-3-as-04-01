00dd001090fff9765f6493ef8e80368e4fb50bd5
Lucas202100646 <202100646@estudantes.ips.pt>
2024-04-03 09:10:48 +0100

    Nivel 2 - Incompleto
    Construtores, gets e sets da classe GroundTransportation.

--- GroundTransportation.java
+++ GroundTransportation.java
@@ -10,4 +10,32 @@ public class GroundTransportation extends Transport
     //Atributos
     private String licensePlate;
     private static final double honorary = 0.03;
+    
+    public GroundTransportation(String licensePlate)
+    {
+        super();
+        
+        if(licensePlate != null || licensePlate == "")
+        {
+            this.licensePlate = licensePlate;
+        }
+        
+        else {licensePlate = "1234";}
+        
+        setFees(honorary);
+    }
+    
+    /** GETS **/
+    
+    public String getLicensePlate()
+    {
+        return licensePlate;
+    }
+    
+    /** SETS **/
+    
+    public void setLicensePlate(String newLicensePlate)
+    {
+        licensePlate = newLicensePlate;
+    }
 }
