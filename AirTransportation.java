d14554b957647bad7988c7d74cff786e3942fd5e
Lucas202100646 <202100646@estudantes.ips.pt>
2024-04-03 08:56:15 +0100

    Nivel 1

---
+++ AirTransportation.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva uma descrição da classe AirTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class AirTransportation extends Transport
+{
+    //Atributos
+    private String name;
+    private int numberOfContainers;
+    private static final double honorary = 0.04;
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,13 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport
+{
+    //Atributos
+    private String licensePlate;
+    private static final double honorary = 0.03;
+}
